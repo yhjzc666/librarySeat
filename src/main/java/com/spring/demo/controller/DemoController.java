1b20f47b8a8e113468396727e430c529fc4d47a8
yuhaojie <1518734749@qq.com>
2022-07-14 22:48:06 +0800

    Initial commit

---
+++ src/main/java/com/spring/demo/controller/DemoController.java
@@ -0,0 +1,8 @@
+package com.spring.demo.controller;
+
+/**
+ * @author yuhaojie on 2022/7/12.
+ * @version 1.0
+ */
+public class DemoController {
+}
---
+++ src/main/java/com/spring/demo/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.spring.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * @author yhj
+ * @description controller层
+ * @date 2022/7/11 17:35
+ */
+@Controller
+@RequestMapping(value = "/hello")
+public class TestController {
+    @RequestMapping(value = "/test")
+    public String test(){
+        return "index";
+    }
+}
